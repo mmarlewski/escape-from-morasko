f1aa8940f2b0fc58d57d704585a087785894a9db
witki11 <wiktor.leszczynski.blue11@gmail.com>
2023-10-11 10:09:59 +0200

    slime boss init

---
+++ core/src/com/efm/entities/bosses/slime/BossSlimeQuarter.kt
@@ -0,0 +1,115 @@
+package com.efm.entities.bosses.slime
+
+import com.badlogic.gdx.audio.Sound
+import com.badlogic.gdx.maps.tiled.TiledMapTile
+import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar
+import com.badlogic.gdx.scenes.scene2d.ui.Stack
+import com.efm.*
+import com.efm.assets.Sounds
+import com.efm.assets.Tiles
+import com.efm.entity.*
+import com.efm.level.World
+import com.efm.room.RoomPosition
+
+class BossSlimeQuarter : Entity, Enemy
+{
+    override val position = RoomPosition()
+    override var maxHealthPoints = 25
+    override var healthPoints = 25
+    override var alive = true
+    override val detectionRange = 1
+    override val attackRange = 1
+    override val stepsInOneTurn = 2
+    override lateinit var healthBar : ProgressBar
+    override lateinit var healthStack : Stack
+    
+    override fun getTile() : TiledMapTile
+    {
+        return Tiles.slimeGreenIdle1
+    }
+    
+    override fun getOutlineYellowTile(n : Int) : TiledMapTile
+    {
+        return when (n)
+        {
+            1    -> Tiles.slimeRedIdle1OutlineYellow
+            2    -> Tiles.slimeRedIdle2OutlineYellow
+            3    -> Tiles.slimeRedIdle1OutlineYellow
+            4    -> Tiles.slimeRedIdle2OutlineYellow
+            else -> Tiles.slimeRedIdle1OutlineYellow
+        }
+    }
+    
+    override fun getOutlineRedTile() : TiledMapTile
+    {
+        return Tiles.slimeRedIdle1OutlineRed
+    }
+    
+    override fun getIdleTile(n : Int) : TiledMapTile?
+    {
+        return when (n)
+        {
+            1    -> Tiles.slimeRedIdle1
+            2    -> Tiles.slimeRedIdle2
+            3    -> Tiles.slimeRedIdle1
+            4    -> Tiles.slimeRedIdle2
+            else -> Tiles.slimeRedIdle1
+        }
+    }
+    
+    override fun getMoveTile(n : Int) : TiledMapTile?
+    {
+        return when (n)
+        {
+            1    -> Tiles.slimeRedIdle1
+            2    -> Tiles.slimeRedIdle2
+            3    -> Tiles.slimeRedIdle1
+            4    -> Tiles.slimeRedIdle2
+            else -> Tiles.slimeRedIdle1
+        }
+    }
+    
+    override fun getAttackTile() : TiledMapTile?
+    {
+        return Tiles.slimeRedAttack
+    }
+    
+    override fun getMoveSound() : Sound?
+    {
+        return Sounds.slimeMove
+    }
+    
+    override fun enemyAttack()
+    {
+        val heroPosition = World.hero.position.copy()
+        val heroDirection = getDirection8(this.position, heroPosition)
+        val swordTile = if (heroDirection == null) null else Tiles.getSwordTile(heroDirection)
+        
+        val animations = mutableListOf<Animation>()
+        
+        animations += Animation.descendTile(swordTile, heroPosition.copy(), 0.2f, 0.25f)
+        animations += Animation.action { playSoundOnce(Sounds.slimeAttack) }
+        animations += Animation.simultaneous(
+                listOf(
+                        Animation.showTile(Tiles.impact, heroPosition.copy(), 0.2f),
+                        Animation.cameraShake(1, 0.5f)
+                      )
+                                            )
+        animations += Animation.action {
+            
+            val attackedPosition = World.hero.position
+            val attackedSpace = World.currentRoom.getSpace(attackedPosition)
+            val attackedEntity = attackedSpace?.getEntity()
+            when (attackedEntity)
+            {
+                is Character ->
+                {
+                    attackedEntity.damageCharacter(10)
+                }
+            }
+        }
+        
+        Animating.executeAnimations(animations)
+    }
+    
+}
