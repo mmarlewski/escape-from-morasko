506a5a626342eeaf9f2aa8d3ebe6a702633b505b
witki11 <wiktor.leszczynski.blue11@gmail.com>
2023-11-14 23:40:13 +0100

    Owerwrite save popup added

--- core/src/com/efm/screens/MenuScreen.kt
+++ core/src/com/efm/screens/MenuScreen.kt
@@ -2,14 +2,13 @@ package com.efm.screens
 
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener
-import com.badlogic.gdx.utils.Scaling
 import com.badlogic.gdx.utils.ScreenUtils
 import com.badlogic.gdx.utils.viewport.ExtendViewport
 import com.efm.*
-import com.efm.assets.*
+import com.efm.assets.Textures
+import com.efm.ui.gameScreen.PopUps
+import com.efm.ui.menuScreen.TitleAndButtons
 
 object MenuScreen : BaseScreen()
 {
@@ -22,126 +21,15 @@ object MenuScreen : BaseScreen()
         // input processor
         super.inputProcessor = stage
         
-        // hud
+        PopUps.display()
+        TitleAndButtons.display()
         
-        val gameTitle = imageOf(
-                Textures.title,
-                Scaling.none
-                               )
-        
-        val playTextButton = textButtonOf(
-                "continue",
-                Fonts.inconsolata30,
-                Colors.black,
-                Textures.upLongNinePatch,
-                Textures.downLongNinePatch,
-                Textures.overNinePatch,
-                Textures.disabledNinePatch,
-                Textures.focusedNinePatch
-                                         )
-        {
-            Sounds.blop.playOnce()
-            playMusicIfNotAlreadyPlaying(Musics.versaLifeAmbient)
-            GameScreen.setScreen()
-        }
-        
-        val startAgainTextButton = textButtonOf(
-                "start again",
-                Fonts.inconsolata30,
-                Colors.black,
-                Textures.upLongNinePatch,
-                Textures.downLongNinePatch,
-                Textures.overNinePatch,
-                Textures.disabledNinePatch,
-                Textures.focusedNinePatch
-                                               )
-        {
-            Sounds.blop.playOnce()
-        }
-        
-        val creditsTextButton = textButtonOf(
-                "credits",
-                Fonts.inconsolata30,
-                Colors.black,
-                Textures.upLongNinePatch,
-                Textures.downLongNinePatch,
-                Textures.overNinePatch,
-                Textures.disabledNinePatch,
-                Textures.focusedNinePatch
-                                            )
-        {
-            Sounds.blop.playOnce()
-            CreditsScreen.setScreen()
-        }
-        
-        val exitButton = imageButtonOf(
-                Textures.exit,
-                Textures.upNinePatch,
-                Textures.downNinePatch,
-                Textures.overNinePatch,
-                Textures.disabledNinePatch,
-                Textures.focusedNinePatch
-                                      )
-        {
-            Sounds.blop.playOnce()
-            exitGame()
-        }
-        val settingsButton = imageButtonOf(
-                Textures.settings,
-                Textures.upNinePatch,
-                Textures.downNinePatch,
-                Textures.overNinePatch,
-                Textures.disabledNinePatch,
-                Textures.focusedNinePatch
-                                          )
-        {
-            Sounds.blop.playOnce()
-            SettingsScreen.setScreen()
-        }
-        
-        val infoButton = imageButtonOf(
-                Textures.info,
-                Textures.upNinePatch,
-                Textures.downNinePatch,
-                Textures.overNinePatch,
-                Textures.disabledNinePatch,
-                Textures.focusedNinePatch
-                                      )
-        {
-            Sounds.blop.playOnce()
-            AppInfoScreen.setScreen()
-        }
-
-        val column = columnOf(
-                rowOf(
-                        gameTitle
-                     ),
-                rowOf(
-                        playTextButton
-                     ),
-                
-                rowOf(
-                        startAgainTextButton
-                     ),
-                rowOf(
-                        creditsTextButton
-                     ),
-                
-                rowOf(
-                        columnOf(settingsButton),
-                        columnOf(infoButton).padLeft(20f).padRight(20f),
-                        columnOf(exitButton)
-                     )
-                             )
-        column.setFillParent(true)
-        stage.addActor(column)
     }
     
     override fun render(delta : Float)
     {
         ScreenUtils.clear(Color.CLEAR)
-    
-        // Draw the background texture region
+        
         EscapeFromMorasko.spriteBatch.begin()
         EscapeFromMorasko.spriteBatch.draw(Textures.mainMenuBackground, 0f, 0f, viewport.worldWidth, viewport.worldHeight)
         EscapeFromMorasko.spriteBatch.end()
--- core/src/com/efm/ui/gameScreen/PopUps.kt
+++ core/src/com/efm/ui/gameScreen/PopUps.kt
@@ -4,7 +4,8 @@ import com.badlogic.gdx.utils.Align
 import com.efm.*
 import com.efm.assets.*
 import com.efm.screens.GameScreen
-import javax.swing.event.ChangeListener
+import com.efm.screens.MenuScreen
+import com.efm.ui.menuScreen.TitleAndButtons
 
 object PopUps
 {
@@ -12,18 +13,35 @@ object PopUps
     lateinit var endTurn : com.badlogic.gdx.scenes.scene2d.ui.Window
     lateinit var menuPause : com.badlogic.gdx.scenes.scene2d.ui.Window
     lateinit var settings : com.badlogic.gdx.scenes.scene2d.ui.Window
+    lateinit var overwriteSave : com.badlogic.gdx.scenes.scene2d.ui.Window
     
     fun setBackgroundVisibility(boolean : Boolean)
     {
         GameScreen.canBeInteractedWith = boolean
         
         RightStructure.setVisibility(boolean)
-    
+        
         ItemsStructure.fillItemsStructureWithItemsAndSkills()
         ItemsStructure.setVisibility(boolean)
-        if(boolean) ItemsStructure.setWeaponDisplay()
+        if (boolean) ItemsStructure.setWeaponDisplay()
         
         LeftStructure.setVisibility(false)
+    }
+    
+    fun overwriteSave() : com.badlogic.gdx.scenes.scene2d.ui.Window
+    {
+        val overwriteSavePopup = windowAreaOf(
+                "You're about to overwrite your\nexisting save. Continue?",
+                Fonts.pixeloid20,
+                Colors.black,
+                Textures.pauseBackgroundNinePatch,
+                { TitleAndButtons.setButtonsVisibility(true) },
+                { TitleAndButtons.setButtonsVisibility(true) }
+                                             )
+        
+        overwriteSavePopup.isVisible = false
+        
+        return overwriteSavePopup
     }
     
     fun endTurn() : com.badlogic.gdx.scenes.scene2d.ui.Window
@@ -32,11 +50,10 @@ object PopUps
                 "End turn?\n\nYou still have some AP left",
                 Fonts.pixeloid20,
                 Colors.black,
-                Textures.pauseBackgroundNinePatch
+                Textures.pauseBackgroundNinePatch,
+                { endCurrentTurn() },
+                {}
                                        )
-        {
-            endCurrentTurn()
-        }
     
         endTurnPopUp.isVisible = false
         
@@ -75,6 +92,12 @@ object PopUps
         endTurn = endTurn()
         menuPause = menuPause()
         settings = settings()
+        overwriteSave = overwriteSave()
+    }
+    
+    fun setOverwriteSaveVisibility(visibility : Boolean)
+    {
+        overwriteSave.isVisible = visibility
     }
     
     fun setEndTurnVisibility(visibility : Boolean)
@@ -97,14 +120,18 @@ object PopUps
         val endTurnWindow = columnOf(rowOf(endTurn)).align(Align.center)
         val pauseWindow = columnOf(rowOf(menuPause)).align(Align.center)
         val settingsWindow = columnOf(rowOf(settings)).align(Align.center)
+        val overwriteSaveWindow = columnOf(rowOf(overwriteSave)).align(Align.center)
+        
         
         settingsWindow.setFillParent(true)
         endTurnWindow.setFillParent(true)
         pauseWindow.setFillParent(true)
+        overwriteSaveWindow.setFillParent(true)
         
         GameScreen.stage.addActor(endTurnWindow)
         GameScreen.stage.addActor(settingsWindow)
         GameScreen.stage.addActor(pauseWindow)
+        MenuScreen.stage.addActor(overwriteSaveWindow)
         
     }
 }
