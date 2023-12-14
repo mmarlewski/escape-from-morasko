97f50e0d5db96449bbb422035e449d9316f78687
tomas <jertom1@st.amu.edu.pl>
2023-12-14 12:05:09 +0100

    small proc gen changes

--- core/src/com/efm/worldGeneration/CreateLevel.kt
+++ core/src/com/efm/worldGeneration/CreateLevel.kt
@@ -1,80 +1,41 @@
-package com.efm.worldGeneration
+import kotlin.math.round
 
-import com.efm.Direction4
-import com.efm.entities.walls.StoneWall
-import com.efm.level.Level
-import com.efm.room.*
-import kotlin.random.Random
-
-class CreateLevel ()
-{
-    fun createLevel() : Level
-    {
-        //get starting room
-        val dungeonMap = Array(10) { IntArray(10) { 0 } }
-        val desiredNbrOfRooms = Random.nextInt(5,8)
-        var rooms : MutableList<Room> = mutableListOf()
-        var createdLevel : Level = Level("1")
-        var posX = Random.nextInt(2, 8)
-        var posY = Random.nextInt(2, 8)
-        dungeonMap[posX][posY] = 1
-        performRandomWalk(desiredNbrOfRooms, posX, posY, dungeonMap)
-        //na podstawie tej macierzy sprawdzać ile powinno być przejść między jakimi pokojami
-        //for loop po dungeonMap, może jakiś słownik, że jaki pokój ma jaki numer i wtedy na
-        //podstawie macierzy brać przejścia
-        //nie da się stworzyć przejścia do pokoju null, więc najpierw same pokoje, potem przejścia między nimi
-        //macierz pozwoli pamiętać jak są względem siebie ułożone i z którego do którego robić przejścia
-        for (i in 0..desiredNbrOfRooms)
-        {
-            val currentRoom = Room((i+1).toString(), Random.nextInt(10, 15), Random.nextInt(10, 15))
-            fillRoomContents(currentRoom)
-            rooms.add(currentRoom)
+fun main() {
+    val mapSize = 100
+    val map = Array(mapSize) { Array(mapSize) { 0 } }
+    
+    partitionMap(map, 0, 0, mapSize, mapSize, 1) // Starting value is 1
+    
+    // Print the partitioned map (just for visualization)
+    for (row in map) {
+        for (cell in row) {
+            print("$cell ")
         }
-        createdLevel.changeStartingRoom(rooms[0])
-        createdLevel.changeStartingPosition(RoomPosition(1, 1))
-        //createdLevel.addRoom()
-        return createdLevel
+        println()
     }
-    
-    private fun fillRoomContents(currentRoom : Room)
-    {
-        for (y in 1 until currentRoom.heightInSpaces) for (x in 1 until currentRoom.widthInSpaces) currentRoom.changeBaseAt(
-                Base.values()[y % 4], x, y)
-        // add walls on left side (facing right)
-                for (y in 1 until currentRoom.heightInSpaces) currentRoom.addEntityAt(StoneWall(Direction4.right), 0, y)
-        // add walls on top side (facing down)
-                for (x in 1 until currentRoom.widthInSpaces) currentRoom.addEntityAt(StoneWall(Direction4.down), x, 0)
-    }
-    
-    fun performRandomWalk(steps: Int, startingX : Int, startingY : Int, dungeonMap : Array<IntArray>)
-    {
-        var currentX = startingX
-        var currentY = startingY
-        
-        for (step in 2 until steps + 2)
-        {
-            var keepLooking = true
-            while(keepLooking)
-            {
-                val directions = listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1)
-                val randomDirection = directions[Random.nextInt(4)]
-    
-                val newX = currentX + randomDirection.first
-                val newY = currentY + randomDirection.second
-    
-                if (newX in 0 until 10 && newY in 0 until 10)
-                {
-                    if (dungeonMap[newX][newY] != 1)
-                    {
-                        currentX = newX
-                        currentY = newY
-                        dungeonMap[currentY][currentX] = step
-                        keepLooking = false
-                    }
-        
-                }
+}
+
+fun partitionMap(map: Array<Array<Int>>, startX: Int, startY: Int, width: Int, height: Int, value: Int) {
+    // Base case: if the area is small enough, assign the same value to all cells
+    if (width <= 2 || height <= 2) {
+        for (i in startX until startX + width) {
+            for (j in startY until startY + height) {
+                map[i][j] = value
             }
-            
         }
+        return
     }
+    
+    // Otherwise, randomly choose a split point for both X and Y dimensions
+    val min = round((width/4).toDouble()).toInt()
+    val max = round((3*(width/4)).toDouble()).toInt()
+    val splitX = startX + (min..max).random()
+    val splitY = startY + (min..max).random()
+    
+    val nextValue = value + 1 // Increase the value for each new segment
+    
+    partitionMap(map, startX, startY, splitX - startX, splitY - startY, nextValue)
+    partitionMap(map, splitX, startY, width - (splitX - startX), splitY - startY, nextValue)
+    partitionMap(map, startX, splitY, splitX - startX, height - (splitY - startY), nextValue)
+    partitionMap(map, splitX, splitY, width - (splitX - startX), height - (splitY - startY), nextValue)
 }
