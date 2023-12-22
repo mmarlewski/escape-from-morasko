55c42ae749f15735e96ade0b0a11a3f51fb3a88c
Dominik Jagosz <domjag5@st.amu.edu.pl>
2023-12-22 22:04:46 +0100

    update AppInfoScreen text

--- core/src/com/efm/screens/AppInfoScreen.kt
+++ core/src/com/efm/screens/AppInfoScreen.kt
@@ -33,28 +33,30 @@ object AppInfoScreen : BaseScreen()
         
         val whatsNewLabel = patchNotesLabel(
                 "What's new?",
-                "- it is now possible to interact with objects such as chests, corpses and NPCs without the need of using movement mode, if the player’s character is next to them.\n" +
-                        "- passages between rooms change visually to show if it is possible to use them\n" +
-                        "- added an option to exit movement mode by pressing movement button or player’s character.\n" +
-                        "- removed “quit game” button \n" +
-                        "- barrel skill added",
+                "- it is now possible to interact with objects such as chests, corpses and NPCs without the need of using movement mode, if the player’s character is next to them,\n" +
+                        "- passages between rooms change visually to show if it is possible to use them,\n" +
+                        "- added an option to exit movement mode by pressing movement button or player’s character,\n" +
+                        "- removed “quit game” button,\n" +
+                        "- added barrel throwing skill,\n" +
+                        "- expanded the tutorial,\n" +
+                        "- added an option to attack enemies with fists.",
                 appInfoTitle.width
                                            )
         
         val bugFixesLabel = patchNotesLabel(
                 "Bug fixes",
-                "- fixed a movement bug that caused players to lose action points, despite not changing player’s position\n" +
-                        "- UI is no longer visible when special event pop-ups are shown\n" +
-                        "- missing elements of UI are now drawn after closing the game and pressing continue\n" +
-                        "- expanded the tutorial",
+                "- fixed a movement bug that caused players to lose action points, despite not changing player’s position,\n" +
+                        "- UI is no longer visible when special event pop-ups are shown,\n" +
+                        "- missing elements of UI are now drawn after closing the game and pressing continue,\n" +
+                        "- the game now ends immediately after the player's death.",
                 appInfoTitle.width
                                            )
         
         val balancingLabel = patchNotesLabel(
                 "Balancing",
-                "- weapons are now more durable\n" +
-                        "- more weapons appear in chests\n" +
-                        "- enemies deal more damage",
+                "- weapons are now more durable,\n" +
+                        "- more weapons appear in chests,\n" +
+                        "- enemies deal more damage.",
                 appInfoTitle.width
                                             )
         
